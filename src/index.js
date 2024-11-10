90b7461916d338e674b2808c9815a634dfa35652
Stephen <65602252+StepOne-ai@users.noreply.github.com>
2024-11-10 20:30:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import bridge from "@vkontakte/vk-bridge";
+import bridge from '@vkontakte/vk-bridge';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Game from "./Game";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game';
 
 // ========================================
-bridge.send("VKWebAppInit", {});
-ReactDOM.render(<Game />, document.getElementById("root"));
+bridge.send('VKWebAppInit', {});
+ReactDOM.render(<Game />, document.getElementById('root'));
